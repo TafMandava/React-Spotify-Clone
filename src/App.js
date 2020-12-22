77ddb6fa399a290b220214b5c1f097681cef2567
Tafadzwa Mandava <tmandava@mezzanineware.com>
2020-12-23 01:55:13 +0200

    Fetching the access token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './Login';
 import './App.css';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  /*
+      Run code based on a given condition
+      Use useEffect to keep an eye on the window location
+  */
+  useEffect(() => {
+     const hash = getTokenFromUrl();
+     const token = hash.access_token;
+     console.log("I have token >>>", token);
+  }, []);
+
   return ( 
     /*
         BEM naming convection
--- src/spotify.js
+++ src/spotify.js
@@ -27,5 +27,25 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
+/*
+    Get the access token from URL
+    http://localhost:3000/#access_token=BQDnh6M26A2zWQgYdlak14gnxRoBa23gPdhwVHMMfCoIpMHqCJUiIVrEhewGFlt3cXXPQPRFZa0QFyQeV-QVyDVm8u7NRVVyMmxOf46KflqOUdGhGodIYKi1L_FkAABmtK_YIChy_N-RLQbVkbD1XnevUtGUvYtQzhosfJQFZV3Zf4FD&token_type=Bearer&expires_in=3600
+*/
+export const getTokenFromUrl = () => {
+    /*
+        Pulling the access token out
+        Go to the URL to the point where we have a hash # 
+    */
+    return window.location.hash
+        .substring(1) // We are chopping the substring and getting the first substring
+        .split('&')  // Split at the amber sign
+        .reduce((initial, item) => {
+            // #access_token=BQDnh6M26A2zWQgYdlak14gnxRoBa23gPdhwVHMMfCoIpMHqCJUiIVrEhewGFlt3cXXPQPRFZa0QFyQeV-QVyDVm8u7NRVVyMmxOf46KflqOUdGhGodIYKi1L_FkAABmtK_YIChy_N-RLQbVkbD1XnevUtGUvYtQzhosfJQFZV3Zf4FD&token_type=Bearer&expires_in=3600
+            let parts = item.split('=');
+            // Go into the initial value that's being returned for the access token  decode the URI Component which is basically the key BQDnh6M26A2zWQgYdlak14gnxRoBa23gPdhwVHMMfCoIpMHqCJUiIVrEhewGFlt3cXXPQPRFZa0QFyQeV-QVyDVm8u7NRVVyMmxOf46KflqOUdGhGodIYKi1L_FkAABmtK_YIChy_N-RLQbVkbD1XnevUtGUvYtQzhosfJQFZV3Zf4FD
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {}) // Get an empty object which is basically what the initial should start with
+}
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
