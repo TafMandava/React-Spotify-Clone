1e6aff17f6a344a141c6e7285e232cb3e95f7ee8
Tafadzwa Mandava <tmandava@mezzanineware.com>
2020-12-26 22:57:10 +0200

    Creating components being rendered in the Player

--- src/Player.js
+++ src/Player.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Sidebar from './Sidebar';
+import Body from './Body';
+import Footer from './Footer';
 import './Player.css';
 
 function Player({ spotify }) {
     return (
         <div className="player">
             <div className="player__body">
-                {/* Sidebar */}            
-                {/* Body */}
+                <Sidebar />           
+                <Body />
             </div>
-            
-            {/* Footer */} 
+
+            <Footer /> 
         </div>
     );
 }
