a494d47a219dcae6e9eadb022005ffc373ef5d43
Tafadzwa Mandava <tmandava@mezzanineware.com>
2020-12-26 21:48:19 +0200

    More informative comments

--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,7 @@ export const initialState = {
     item: null,
     /*  If you do not want to constantly login the app
         Set token to 'BQBdXk-JBahehBsscWDWwsy8kPYfUpxEmGyRbmLecMkUuzoL2DodxZ7pd-wOyeN7bU2PmwieWBmgIEoANF06-jrL5xINOOt6GEMwC_zWdGiLOjQjd6K8oTOttC1JRmgBeYvnSXNFlwWd30Pt3Wcx0IHMAlJfZDqBGH6Koycs6L7SmnHU' when developing app and remove when done
+        However, this should be removed when the application is production ready
     */
     token: null
 }
